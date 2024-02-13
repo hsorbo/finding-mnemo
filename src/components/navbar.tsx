b6309d2bbdcdd922fb8e4977df6d571abbc58853
Andreas <andreas@ros.land>
2024-02-13 10:43:13 -0500

    change navbar name to Finding MNemo!

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = ({imports}: {imports:Import[]}) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Yolo!</a>
+                <a className="navbar-brand" href="#">Finding MNemo!</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
